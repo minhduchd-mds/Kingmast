fd8658b38a444098a2fa2569c65fc36627212ac9
Đỗ Minh Đức <29146304+minhduchd-mds@users.noreply.github.com>
2026-09-12 15:33:58 +0700

    feat(hmi): switch ESP32 live bench to premium cockpit UI

--- apps/hmi/app/esp32-live/page.tsx
+++ apps/hmi/app/esp32-live/page.tsx
@@ -1,13 +1,5 @@
-import KingmastV006 from '../../components/KingmastV006';
-import NextgenIntelligenceHud from '../../components/NextgenIntelligenceHud';
-import { Esp32C3BenchBridge } from '../../components/Esp32C3BenchBridge';
+import KingmastCockpitPro from '../../components/KingmastCockpitPro';
 
 export default function Esp32LiveBenchPage() {
-  return (
-    <>
-      <Esp32C3BenchBridge />
-      <KingmastV006 />
-      <NextgenIntelligenceHud />
-    </>
-  );
+  return <KingmastCockpitPro />;
 }
